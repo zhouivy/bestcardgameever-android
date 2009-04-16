7aa771a588b9eb96a07a7dd206ba18468a44726a
eladkatz <eladkatz@8b6896de-2235-11de-8297-c3254a27cc66>
2009-04-16 23:52:34 +0000

    completed verifyYaniv alogrithm UNTESTED!!! another temp check-in

---
+++ trunk/ bestcardgameever-android/Yan-Droid/src/com/sheepzkeen/yaniv/InvalidYanivException.java
@@ -0,0 +1,13 @@
+package com.sheepzkeen.yaniv;
+
+public class InvalidYanivException extends Exception {
+
+	/**
+	 * 
+	 */
+	public InvalidYanivException(String message) {
+		super(message);
+	}
+	private static final long serialVersionUID = -2690350835355321831L;
+
+}
--- trunk/ bestcardgameever-android/Yan-Droid/src/com/sheepzkeen/yaniv/PlayerHand.java
+++ trunk/ bestcardgameever-android/Yan-Droid/src/com/sheepzkeen/yaniv/PlayerHand.java
@@ -3,6 +3,9 @@ package com.sheepzkeen.yaniv;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
 
 import android.view.View;
 import android.widget.ImageView;
@@ -23,7 +26,7 @@ public class PlayerHand extends Hand {
 
 	//should Override
 	@Override
-	protected void selectCardsToDrop() {
+	protected void selectCardsToDrop() throws InvalidYanivException {
 		//Do nothing here, selection was performed by the drop action
 		verifyCardsToDrop();
 	}
@@ -34,7 +37,7 @@ public class PlayerHand extends Hand {
 
 
 
-	private void verifyCardsToDrop() {
+	private void verifyCardsToDrop() throws InvalidYanivException {
 		
 		//Verify algorithm:
 		// 1) remove jokers, add 1 to jokerCount for each joker
@@ -49,29 +52,88 @@ public class PlayerHand extends Hand {
 		// 4.	4.	1) if this card and the next card have a difference of exactly 1 - continue, else:
 		// 4.	4.	1.	2) (if difference>1) check that difference-1 <= jokerCount, if so, subtract difference-1 from jokerCount
 		// 5) see if ace existed, if so, check if it can be attached to end\start (check highest card + jokercount == 14 or lowest card - jokerCount == 1)
+
+		//0 - initialize
 		int jokerCount = 0;
-		Character currentSuit = null;
-		boolean allCardsAreSameSuit = true;
+		boolean suitsAreDifferent = false;
+		Set<Character> suitsInCards = new HashSet<Character>();
+		Set<PlayingCard> acesInCards = new HashSet<PlayingCard>();
 		ArrayList<PlayingCard> cardsToCheck  = (ArrayList<PlayingCard>) Arrays.asList(cards);
+		//1
 		for (PlayingCard card : cardsToCheck) {
 			if(card.getIntegerValue() == null){
 				jokerCount++;
 				cardsToCheck.remove(card);
-			}else{
-				if(allCardsAreSameSuit && (card.getSuit() == currentSuit || currentSuit == null))
-				{
-					currentSuit = card.getSuit();
-				}else{
-					allCardsAreSameSuit = false;
+			}
+		}
+		//2
+		if(cardsToCheck.size()<=1){
+			//allow
+			return;
+		}
+		//3	
+		for (PlayingCard card : cardsToCheck) {
+			if(suitsInCards.contains(card.getSuit())){
+				//this suit has already appeared once
+				suitsAreDifferent = true;
+			}
+		}
+		
+		//3.1
+		PlayingCard lastCardChecked = null;
+		if(suitsAreDifferent){
+			for (PlayingCard card : cardsToCheck) {
+				if(lastCardChecked != null && lastCardChecked.getIntegerValue() != card.getIntegerValue())
+					//Reject
+					throw new InvalidYanivException("Cards of different suits must have same value!");
+			}
+		}else{
+			//4
+			//4.1
+			if(cardsToCheck.size() < 3 && jokerCount == 0){
+				//Reject
+				throw new InvalidYanivException("Cards have same suit, but are not enough to complete a series! " +
+						"(only " + cardsToCheck.size() + " cards dropped and no jokers)");
+			}
+			//4.2
+			for (PlayingCard card : cardsToCheck) {
+				if(card.getIntegerValue() == PlayingCard.ACE){
+					acesInCards.add(card);
+					cardsToCheck.remove(card);
+				}
+			}
+			//4.3
+			Collections.sort(cardsToCheck);
+			//4.4
+			for (int i = 0; i < cardsToCheck.size()-1; i++) {
+				int differenceBetweenThisAndNextCardVal =
+					cardsToCheck.get(i).getIntegerValue() - cardsToCheck.get(i+1).getIntegerValue();
+				//4.4.1
+				if( differenceBetweenThisAndNextCardVal != 1){
+					//4.4.1.2
+					if(differenceBetweenThisAndNextCardVal - 1 > jokerCount){
+						//reject
+						throw new InvalidYanivException("Cards have same suit, " +
+								"but the difference between them is too big, even with jokers. " +
+								"(cards: "+ cardsToCheck.get(i) + " and " + cardsToCheck.get(i+1)+")");
+					}else{
+						jokerCount = jokerCount - differenceBetweenThisAndNextCardVal - 1;
+					}
 				}
 					
 			}
-			
-			
+		}		
+		//5
+		if(!acesInCards.isEmpty()){
+			//highest
+			if((cardsToCheck.get(0).getIntegerValue() +jokerCount == 14) ||
+					(cardsToCheck.get(cardsToCheck.size()).getIntegerValue() -jokerCount == 1)){
+				//OK
+			}else{
+				//reject
+				throw new InvalidYanivException("Cards have at least one ace that cannot be attached to series.");
+			}
 		}
-		Collections.sort(cardsToCheck);
-		
-		
 	}
 
 
